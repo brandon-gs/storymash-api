87c741989cc45a61148d2637a7cf94f5efbc1fd8
BrandonGS <brandongs180@gmail.com>
2022-09-23 01:40:47 -0500

    update default mongodb uri

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const { MONGO_URI = "mongodb://localhost/storymash-api" } = process.env;
+const { MONGO_URI = "mongodb://127.0.0.1:27017/stm-test-api" } = process.env;
 
 export const client = new MongoClient(MONGO_URI);
 export const db = client.db();
