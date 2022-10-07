3a3559e1c3d66845ac3d1f64042176298fd5ec17
BrandonGS <brandongs180@gmail.com>
2022-10-07 14:56:34 -0500

    update zod error messages

--- src/errorMessages.ts
+++ src/errorMessages.ts
@@ -1,5 +1,7 @@
 export const ErrorMessages = {
   required_error: "Este campo es requerido",
   min: (minChars: number) =>
-    `Este campo debe tener al menos ${minChars} caracteres`,
+    minChars > 1
+      ? `Este campo debe tener al menos ${minChars} caracteres`
+      : "Este campo es requerido",
 };
